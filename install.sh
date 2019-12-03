6ca7038234e46ff233b05ba680573c14551a3be3
archetype2142 <malhotraritwick2011@gmail.com>
2019-12-03 14:39:24 +0100

    final hopefully

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:george-edison55/cmake-3.x
 sudo apt -y update
 sudo apt -y install cmake
+sudo apt-get install netdiscover
 
 sudo apt -y install libwebsockets-dev g++ pkg-config git vim-common libjson-c-dev libssl-dev
 git clone https://github.com/tsl0922/ttyd.git
@@ -16,14 +17,16 @@ cd
 
 PORT="$(dmesg | grep 'pl2303 converter now attached to' | grep -o 'tty.*')"
 
-touch run.sh
-cat > run.sh <<EOF
+touch run_console
+cat > run_console <<EOF
 #!/bin/bash
 sudo kill -9 $(ps aux | grep 'ttyd' | awk '{print $2}')
+PORT="$(dmesg | grep 'pl2303 converter now attached to' | grep -o 'tty.*')"
 sudo ttyd -o picocom -b 9600 /dev/$PORT
 EOF
 
-sudo chmod +x run.sh
+sudo chmod +x run_console
+sudo mv run_console /usr/local/bin/ttyd
 
 wget https://github.com/archetype2142/onremote-console-access/raw/master/picocom
 sudo chmod +x picocom 
