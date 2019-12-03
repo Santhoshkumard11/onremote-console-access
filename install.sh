8613723bb57680b27f0f000bb66e750e37bf325f
archetype2142 <malhotraritwick2011@gmail.com>
2019-12-03 14:54:50 +0100

    hope

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
-# sudo apt install build-essential
+# !/bin/bash
+sudo apt install build-essential
 
-# sudo apt -y install software-properties-common
-# sudo add-apt-repository ppa:george-edison55/cmake-3.x
-# sudo apt -y update
-# sudo apt -y install cmake
-# sudo apt-get install netdiscover
+sudo apt -y install software-properties-common
+sudo add-apt-repository ppa:george-edison55/cmake-3.x
+sudo apt -y update
+sudo apt -y install cmake
+sudo apt-get install netdiscover
 
-# sudo apt -y install libwebsockets-dev g++ pkg-config git vim-common libjson-c-dev libssl-dev
-# git clone https://github.com/tsl0922/ttyd.git
-# cd ttyd && mkdir build && cd build
-# cmake ..
-# sudo make && make install
-# sudo cp ttyd /usr/local/bin/ttyd
-# cd 
+sudo apt -y install libwebsockets-dev g++ pkg-config git vim-common libjson-c-dev libssl-dev
+git clone https://github.com/tsl0922/ttyd.git
+cd ttyd && mkdir build && cd build
+cmake ..
+sudo make && make install
+sudo cp ttyd /usr/local/bin/ttyd
+cd 
 
-# PORT="$(dmesg | grep 'pl2303 converter now attached to' | grep -o 'tty.*')"
+PORT="$(dmesg | grep 'pl2303 converter now attached to' | grep -o 'tty.*')"
 
 touch run_console
 cat > run_console <<EOF
@@ -29,8 +29,8 @@ sudo chmod +x run_console
 cd
 sudo mv run_console /usr/local/bin/
 
-# wget https://github.com/archetype2142/onremote-console-access/raw/master/picocom
-# sudo chmod +x picocom 
-# sudo mv picocom /usr/local/bin
-# sudo rm -rv ttyd
+wget https://github.com/archetype2142/onremote-console-access/raw/master/picocom
+sudo chmod +x picocom 
+sudo mv picocom /usr/local/bin
+sudo rm -rv ttyd
 rm -- "$0"
